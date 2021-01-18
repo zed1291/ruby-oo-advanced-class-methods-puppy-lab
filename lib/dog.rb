5afeccdfabac0e8ba85ff7fc20767c9d0ac99c92
“zed” <zed@kolk.me>
2021-01-18 15:00:19 -0600

    finished

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
-# Add your code here
+require 'pry'
+
+class Dog
+    attr_accessor :name
+    @@all = []
+    def initialize name
+        @name = name
+        self.save
+
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all = []
+    end
+
+    def self.print_all
+        @@all.each {|dogs| puts dogs.name}
+    end
+
+    def save
+        @@all << self
+    end
+
+end
